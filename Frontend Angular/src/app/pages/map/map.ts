8c8498a8c8d72f1a9fb7d7010764fa9a9df4c6e1
Yoel ILLOUZ <yoel.illouz@alten.com>
2026-04-22 11:52:59 +0200

    ajout de commentaires clairs sur le component/page : Map (map.html, map.ts et map.scss)

--- Frontend Angular/src/app/pages/map/map.ts
+++ Frontend Angular/src/app/pages/map/map.ts
@@ -9,19 +9,24 @@ import * as L from 'leaflet';
 })
 export class Map implements AfterViewInit {
 
+  // propriété de type L.Map qui est le type dans Leaflet pour afficher la carte:
   maCarte!: L.Map;
 
   // charger la carte Leaflet, uniquement une fois que le DOM Html est chargé (car Leaflet à besoin du DOM Html)
   ngAfterViewInit(): void {
 
+    // appel de la fonction pour afficher la carte, définis juste en dessous
     this.afficherLaCarte();
 
   }
 
+  // fonction qui crée la carte Leaflet dans le div : id="divMaCarte" (dans le map.html)
   afficherLaCarte(): void {
 
+    // création de la carte pointé sur les chutes du Niagara par défaut:
     this.maCarte = L.map('divMaCarte').setView([43.0799, -79.0747], 15);
 
+    // ajout des tuiles de la carte du monde en fond d'écran de la carte Leaflet: 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(this.maCarte);
     
   }
